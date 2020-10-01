307da3cb74d643d64ec1e66e8d6708ab742fc35c
sbertix <stefano.bertagno@gmail.com>
2020-10-01 13:47:30 +0200

    Add custom status description

--- Sources/Swiftchain/Error.swift
+++ Sources/Swiftchain/Error.swift
@@ -9,10 +9,23 @@ import Foundation
 
 public extension Keychain {
     /// An `enum` holding reference to keychain specific `Error`s.
-    enum Error: Swift.Error {
+    enum Error: LocalizedError {
         /// A keychain specific error.
         case status(OSStatus)
         /// Invalid downcast.
         case invalidCasting
+
+        /// The error description.
+        public var errorDescription: String? {
+            switch self {
+            case .status(let status):
+                if #available(iOS 11.3, tvOS 11.3, watchOS 4.3, *) {
+                    return SecCopyErrorMessageString(status, nil) as String? ?? String(describing: status)
+                } else {
+                    return String(describing: status)
+                }
+            case .invalidCasting: return "Invalid type casting."
+            }
+        }
     }
 }
